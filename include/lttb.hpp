06d03511145bdfe7e64af9e88b3dc8517c34d5d9
parkertomatoes <parkertomatoes@gmail.com>
2019-12-05 12:25:33 -0500

    Move point template parameters to a class template

--- include/lttb.hpp
+++ include/lttb.hpp
@@ -2,69 +2,73 @@
 #include <cmath>
 #include <algorithm>
 
-template <typename InputIt, typename OutputIt, typename TPoint, typename TData, TData TPoint::*x, TData TPoint::*y>
-void LargestTriangleThreeBuckets(InputIt source, size_t sourceSize, OutputIt destination, size_t destinationSize)
+template <typename TPoint, typename TData, TData TPoint::*x, TData TPoint::*y>
+struct LargestTriangleThreeBuckets
 {
-    if (destinationSize == 0)
+    template <typename InputIt, typename OutputIt>
+    static void Downsample(InputIt source, size_t sourceSize, OutputIt destination, size_t destinationSize)
     {
-        return;
-    }
-
-    if (destinationSize >= sourceSize)
-    {
-        std::copy_n(source, sourceSize, destination);
-    }
-
-    *destination = *source;
-    ++destination;
-    
-    TData every = static_cast<TData>(sourceSize - 2) / (destinationSize - 2);
-    size_t aIndex = 0;
-
-    for (size_t i = 0; i < destinationSize - 2; ++i)
-    {
-        size_t avgRangeStart = static_cast<size_t>((i + 1) * every) + 1;
-        size_t avgRangeEnd = static_cast<size_t>((i + 2) * every) + 1;
-        if (avgRangeEnd > sourceSize)
+        if (destinationSize == 0)
         {
-            avgRangeEnd = sourceSize;
+            return;
         }
 
-        TData avgRangeLength = avgRangeEnd - avgRangeStart;
-        TData avgX = 0;
-        TData avgY = 0;
-        for (; avgRangeStart < avgRangeEnd; ++avgRangeStart)
+        if (destinationSize >= sourceSize)
         {
-            avgX += source[avgRangeStart].*x;
-            avgY += source[avgRangeStart].*y;
-        }
-        avgX /= avgRangeLength;
-        avgY /= avgRangeLength;
-
-        size_t rangeOffs = static_cast<size_t>(i * every) + 1;
-        size_t rangeTo = static_cast<size_t>((i + 1) * every) + 1;
-        TData pointAX = source[aIndex].*x;
-        TData pointAY = source[aIndex].*y;
-
-        TData maxArea = -1;
-        size_t nextAIndex = 0;
-        for (; rangeOffs < rangeTo; ++rangeOffs)
-        {
-            TData area = std::abs(
-                (pointAX - avgX) * (source[rangeOffs].*y - pointAY) -
-                (pointAX - source[rangeOffs].*x) * (avgY - pointAY)) / 2;
-            if (area > maxArea) 
-            {
-                maxArea = area;
-                nextAIndex = rangeOffs;
-            }
+            std::copy_n(source, sourceSize, destination);
         }
 
-        *destination = source[nextAIndex];
+        *destination = *source;
         ++destination;
-        aIndex = nextAIndex;
-    }
+        
+        TData every = static_cast<TData>(sourceSize - 2) / (destinationSize - 2);
+        size_t aIndex = 0;
 
-    *destination = source[sourceSize-1];
-    ++destination;
-}
+        for (size_t i = 0; i < destinationSize - 2; ++i)
+        {
+            size_t avgRangeStart = static_cast<size_t>((i + 1) * every) + 1;
+            size_t avgRangeEnd = static_cast<size_t>((i + 2) * every) + 1;
+            if (avgRangeEnd > sourceSize)
+            {
+                avgRangeEnd = sourceSize;
+            }
+
+            TData avgRangeLength = avgRangeEnd - avgRangeStart;
+            TData avgX = 0;
+            TData avgY = 0;
+            for (; avgRangeStart < avgRangeEnd; ++avgRangeStart)
+            {
+                avgX += source[avgRangeStart].*x;
+                avgY += source[avgRangeStart].*y;
+            }
+            avgX /= avgRangeLength;
+            avgY /= avgRangeLength;
+
+            size_t rangeOffs = static_cast<size_t>(i * every) + 1;
+            size_t rangeTo = static_cast<size_t>((i + 1) * every) + 1;
+            TData pointAX = source[aIndex].*x;
+            TData pointAY = source[aIndex].*y;
+
+            TData maxArea = -1;
+            size_t nextAIndex = 0;
+            for (; rangeOffs < rangeTo; ++rangeOffs)
+            {
+                TData area = std::abs(
+                    (pointAX - avgX) * (source[rangeOffs].*y - pointAY) -
+                    (pointAX - source[rangeOffs].*x) * (avgY - pointAY)) / 2;
+                if (area > maxArea) 
+                {
+                    maxArea = area;
+                    nextAIndex = rangeOffs;
+                }
+            }
+
+            *destination = source[nextAIndex];
+            ++destination;
+            aIndex = nextAIndex;
+        }
+
+        *destination = source[sourceSize-1];
+        ++destination;
+    }
+};
